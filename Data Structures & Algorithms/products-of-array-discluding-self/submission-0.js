96a4ca85e9a5f0fdacb179d8a327e3d0e248d81b
Mansi Verma <134773916+Mansiverma30@users.noreply.github.com>
2026-07-15 18:11:52 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let rightProduct = 1;
+        let answer = []
+        answer[0] = 1;
+
+        for (let i = 1; i < nums.length; i++) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+        for (let i = nums.length - 1; i >= 0; i--) {
+            answer[i] = answer[i] * rightProduct;
+            rightProduct *= nums[i];
+        }
+        return answer
+    }
+}
