7d5722eb39292a6c8610c8eaed0d793073d6d537
Mansi Verma <134773916+Mansiverma30@users.noreply.github.com>
2026-07-19 11:21:09 +0530

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        let idx1 = 0;
+        let idx2 = numbers.length - 1;
+        while (idx1 < idx2) {
+            console.log(idx1, idx2);
+            if (numbers[idx1] + numbers[idx2] === target) {
+                return [idx1 + 1, idx2 + 1];
+            } else if (numbers[idx1] + numbers[idx2] < target) {
+                idx1++;
+            } else {
+                idx2--;
+            }
+        }
+    }
+}
