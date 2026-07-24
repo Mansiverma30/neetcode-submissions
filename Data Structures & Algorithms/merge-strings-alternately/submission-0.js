bcc8e4640be3354c855de73aaf3fef844ee25de3
Mansi Verma <134773916+Mansiverma30@users.noreply.github.com>
2026-07-24 16:35:33 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {string}
+     */
+    mergeAlternately(s, t) {
+        let i = 0;
+        let j = 0;
+        let ans = [];
+        while (i < s.length) {
+            ans.push(s[i]);
+            ans.push(t[j]);
+            i++;
+            j++;
+        }
+        if (j !== t.length) {
+            let k = t.slice(j, t.length);
+            ans.push(k);
+        }
+        return ans.join("");
+    }
+}
