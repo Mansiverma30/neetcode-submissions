41a4091660f119516fad11306c5ead48a37afb18
Mansi Verma <134773916+Mansiverma30@users.noreply.github.com>
2026-07-29 12:00:11 +0530

    Add: evaluate-reverse-polish-notation - submission-1

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        let stack = [];
+        for (const token of tokens) {
+            if (token === "+" || token === "-" || token === "*" || token === "/") {
+                let right = stack.pop();
+                let left = stack.pop();
+                if (token === "+") {
+                    stack.push(left + right);
+                } else if (token === "-") {
+                    stack.push(left - right);
+                } else if (token === "*") {
+                    stack.push(left * right);
+                } else {
+                    stack.push(Math.trunc(left / right));
+                }
+            } else stack.push(Number(token));
+        }
+        return stack[0];
+    }
+}
